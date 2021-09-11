c35104768dacca8511ab32004229a9571997c4f3
Stian <60408983+Strand-Made@users.noreply.github.com>
2021-09-11 20:37:39 +0200

    checkout page partly done

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import Header from "../components/Header";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import { currencyFormatter } from "../helpers/currencyHelper";
+import { useSession } from "next-auth/client";
 
 const Checkout = () => {
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const [session] = useSession();
   return (
     <div className="bg-gray-100">
       <Header />
@@ -28,7 +32,25 @@ const Checkout = () => {
           </div>
         </div>
         {/* right  */}
-        <div></div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 ? (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length} items):{" "}
+                <span className="font-bold"> {currencyFormatter(total)}</span>
+                <button
+                  className={`button mt-2 ${
+                    !session
+                      ? "from-gray-300 to-gray-500 border-gray-200 text-gray-300"
+                      : null
+                  }`}
+                >
+                  {!session ? "Sign in" : "Proceed to checkout"}
+                </button>
+              </h2>
+            </>
+          ) : null}
+        </div>
       </main>
     </div>
   );
